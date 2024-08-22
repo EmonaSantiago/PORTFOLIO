c81d00a76efa2c3ff1d0d7fde6b430a25a25034d
Emona Santiago <emonasantiago1207@gmail.com>
2024-08-22 11:12:09 +0200

    homepage pushed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class= 'container'>
+    <div class="container_content">
+          <div class="container_content_text">
+              <div class="title">
+                  <h1><span>Emona</span> Santiago</h1>
+              </div>
+              <div class="introduction">
+                <p>
+                  Welkom op mijn portfolio website! Hier krijg je een voorproefje van mijn creatieve en technische projecten. Neem een kijkje bij mijn GitHub-projecten, waar ik mijn passie voor code en innovatie deel. Veel plezier met ontdekken!
+                </p>
+              </div>
+              <div class="button">
+                  <a class="button_more" href="/projects" >Ga verder</a>
+              </div>
+          </div>
+    </div>
+    
+    <div class="container_outer_image">
+        <div class="image">
+            <img src="../emona.png" alt="emona" width="100%">
+        </div>
+    </div>
+</div>
+    
+<div class="first-page-animation"></div>
