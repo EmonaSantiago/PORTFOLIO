4501b2008069f667e791132059c537444e5e3582
Emona Santiago <emonasantiago1207@gmail.com>
2024-08-24 19:34:08 +0200

    code cleaning

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -12,6 +12,7 @@
 		<main>
 			<div class="content">
 				<h1>GITHUB PROJECTS</h1>
+				<p class="introduction_projects">Welkom op mijn portfolio van GitHub-projecten! Hier deel ik enkele van de codeprojecten. <br>Klik gerust door om meer te ontdekken! </p>
 				<!-- Import Cards section -->
 			<Card></Card>
 			</div>
